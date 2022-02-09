3851b5d59d2d4b0c86996a6c64d7a9313007aef2
Alessandro Festa <bringyourownid@gmail.com>
2022-02-09 11:57:24 +0100

    test

--- tfx_demo.py
+++ tfx_demo.py
@@ -1,30 +1,10 @@
-import pkg_resources
-import importlib
-importlib.reload(pkg_resources)
+import pandas as pd 
+import numpy as np
 
-import tensorflow as tf
-import tensorflow_data_validation as tfdv
-print('TF version:', tf.__version__)
-print('TFDV version:', tfdv.version.__version__)
 
-import os
-import tempfile, urllib, zipfile
+df = pd.DataFrame(
+  np.random.randint(0, 5, size=(100, 4)), 
+  columns=[f'column{i}' for i in range(0, 4)]
+)
 
-# Set up some globals for our file paths
-BASE_DIR = tempfile.mkdtemp()
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-OUTPUT_DIR = os.path.join(BASE_DIR, 'chicago_taxi_output')
-TRAIN_DATA = os.path.join(DATA_DIR, 'train', 'data.csv')
-EVAL_DATA = os.path.join(DATA_DIR, 'eval', 'data.csv')
-SERVING_DATA = os.path.join(DATA_DIR, 'serving', 'data.csv')
-
-# Download the zip file from GCP and unzip it
-zip, headers = urllib.request.urlretrieve('https://storage.googleapis.com/artifacts.tfx-oss-public.appspot.com/datasets/chicago_data.zip')
-zipfile.ZipFile(zip).extractall(BASE_DIR)
-zipfile.ZipFile(zip).close()
-
-train_stats = tfdv.generate_statistics_from_csv(data_location=TRAIN_DATA)
-
-# docs-infra: no-execute
-tfdv.visualize_statistics(train_stats)
-
+print(df)
